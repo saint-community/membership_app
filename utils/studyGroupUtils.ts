ef472229834733e138b8ab4a470117176544dbdc
MDWeena <akpologun.winner@gmail.com>
2025-08-13 15:38:50 +0100

    feat: enhance AssignmentDetail component with detailed view and navigation

--- utils/studyGroupUtils.ts
+++ utils/studyGroupUtils.ts
@@ -8,7 +8,6 @@ export function cleanDateString(dateStr: string): string {
     .replace(/,\s*/g, ' ')
     .trim();
 }
-
 export const isLate = (dueDate: string, submitDate: string) => {
   console.log('dueDate', dueDate, 'submitDate', submitDate);
   const cleanDue = cleanDateString(dueDate);
@@ -20,4 +19,34 @@ export const isLate = (dueDate: string, submitDate: string) => {
 
   console.log('Parsed due:', due.toISOString(), 'submit:', submit.toISOString());
   return submit.isAfter(due);
+};
+const getOrdinal = (day: number) => {
+  if (day > 3 && day < 21) return `${day}th`; // 4th - 20th
+  switch (day % 10) {
+    case 1:
+      return `${day}st`;
+    case 2:
+      return `${day}nd`;
+    case 3:
+      return `${day}rd`;
+    default:
+      return `${day}th`;
+  }
+};
+export const getStudyGroupDateRange = (dueDate: string) => {
+  console.log('Due Date:', dueDate);
+
+  const cleaned = cleanDateString(dueDate);
+  console.log('Cleaned Due Date:', cleaned);
+
+  const end = dayjs(cleaned, 'DD MMMM YYYY');
+  const start = end.subtract(7, 'day');
+
+  const sameMonth = start.month() === end.month();
+
+  if (sameMonth) {
+    return `${getOrdinal(start.date())} - ${getOrdinal(end.date())} ${end.format('MMMM, YYYY')}`;
+  } else {
+    return `${getOrdinal(start.date())} ${start.format('MMMM, YYYY')} - ${getOrdinal(end.date())} ${end.format('MMMM, YYYY')}`;
+  }
 };
